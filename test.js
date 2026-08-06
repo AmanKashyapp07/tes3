7dbacf787099589e65898f4a39ab0265e2704357
AMAN KASHYAP <iit2024140@iiita.ac.in>
2026-08-07 03:32:42 +0530

    Initial commit

---
+++ test.js
@@ -0,0 +1,12 @@
+const { add, multiply } = require('./index');
+const assert = require('assert');
+
+console.log('🧪 Running automated unit tests...');
+
+assert.strictEqual(add(2, 3), 5, '2 + 3 should equal 5');
+assert.strictEqual(multiply(4, 5), 20, '4 * 5 should equal 20');
+assert.strictEqual(add(-1, 1), 0, '-1 + 1 should equal 0')
+assert.strictEqual(multiply(0, 10), 0, '0 * 10 should equal 0');
+
+console.log('✅ All unit tests passed seamlessly!');
+//gihgkj\/fdkj/sdfsdacadfdjhsdbf
