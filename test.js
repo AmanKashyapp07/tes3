364357c9b56df58e8bcd3a1acbcc3dd3833cf693
AMAN KASHYAP <iit2024140@iiita.ac.in>
2026-08-07 03:36:00 +0530

    asd

--- test.js
+++ test.js
@@ -8,6 +8,7 @@ assert.strictEqual(multiply(4, 5), 20, '4 * 5 should equal 20');
 assert.strictEqual(add(-1, 1), 0, '-1 + 1 should equal 0')
 assert.strictEqual(multiply(0, 10), 0, '0 * 10 should equal 0');
 assert.strictEqual(add(0, 0), 0, '0 + 0 should equal 0');
+assert.strictEqual(multiply(-2, -3), 6, '-2 * -3 should equal 6');
 
 console.log('✅ All unit tests passed seamlessly!');
 //gihgkj\/fdkj/sdfsdacadfdjhsdbf
